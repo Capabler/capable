28c05aa3f544e6599dfe30a029b95dd3585fbe07
topthinking <topthinking@ximalaya.com>
2018-10-23 14:10:27 +0800

    new

---
+++ example/controllers/index.js
@@ -0,0 +1,27 @@
+module.exports = class extends MY_Controller { 
+	
+	async index() {
+		await this.method('post', 'get')
+		
+		await this.method.get(async () => {
+			let data = {
+				isLogin: this.isLogin
+			}
+			this.load.model('user_model')
+			this.load.model('book_model')
+			data['userList'] = await this.user_model.getuserList()
+			data['bookList'] = await this.book_model.bookList()
+
+			//默认模板引擎是ejs，动态加载react模板引擎，用来渲染该页面
+			if ($_SESSION['user_name'] === 'root') {
+				// 只有 root 用户登录使用该引擎
+				this.load.template('react')
+			}
+			await this.view('index', data)
+		})
+
+		await this.method.post(async () => { 
+			this.ctx.body = '123'
+		})
+	}		
+}
---
+++ example/core/MY_Controller.js
@@ -0,0 +1,32 @@
+/**
+ * 公共的控制器
+ * 处理一些初始化的信息，比如登录信息，统一返回等
+ */
+module.exports = class extends DJ_Controller { 
+	constructor(ctx) { 
+		super(ctx)		
+		this.isAjax = false
+		if (/^\/api/.test(ctx.request.url)) { 
+			//处理请求是否是通过正常的ajax访问的
+			if (ctx.request.headers["x-requested-with"] === 'XMLHttpRequest') {
+				this.isAjax = true	
+			}
+		}
+		this.isLogin = $_SESSION.hasOwnProperty('user_name') && $_SESSION['user_name'] ? true : false
+
+		this.MethodNotAllowed(() => {
+			this.ctx.status = 405
+			this.ctx.body = 'Method Not Allowed'
+		})
+	}
+
+	sendJSON(status = -1, message = 'fail', data = []) {
+		this.ctx.set('Content-Type', 'text/json')	
+		this.ctx.body = JSON.stringify({
+			status,
+			message,
+			data
+		})
+	}
+
+}
