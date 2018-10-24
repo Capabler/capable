65a10e51a263e5cbb071cddaa1920c28bd97daa3
topthinking <topthinking@ximalaya.com>
2018-10-24 16:11:46 +0800

    1

--- lib/database/sequelize/index.ts
+++ lib/database/sequelize/index.ts
@@ -8,12 +8,14 @@ export default class extends Base {
   private columns: any;
   private table: any;
   private sql: any;
+  private conditions: Object;
 
   constructor(config = {}) {
     super(config);
     this.columns = '';
     this.table = '';
     this.sql = '';
+    this.conditions = {};
   }
 
   public async query(sql = '') {
@@ -23,6 +25,11 @@ export default class extends Base {
 
   public select(columns = '') {
     this.columns = columns;
+    return this;
+  }
+
+  public where(conditions = {}) {
+    this.conditions = conditions;
     return this;
   }
 
@@ -38,19 +45,25 @@ export default class extends Base {
       } else {
         // this.db.select('version,token').from('info').get().row_array()
         const columns = this.columns ? '*' : this.columns;
-        this.sql = 'SELECT ' + columns + ' FROM ' + this.table;
-        return this;
+        this.sql = 'SELECT ' + columns + ' FROM ' + this.table + ' WHERE ';
       }
     } else {
       // this.db.get('info').row_array()
-      this.sql = 'SELECT * FROM ' + table;
-      return this;
+      this.sql = 'SELECT * FROM ' + table + ' WHERE ';
     }
+
+    for (const key in this.conditions) {
+      if (this.conditions.hasOwnProperty(key)) {
+        this.sql += key + ' = "' + (this as any).conditions[key] + '" AND ';
+      }
+    }
+    this.sql += ' 1 =1 ';
+    return this;
   }
 
   public async row_array() {
     const result = await this.query();
-    return result[0];
+    return result.length ? result[0] : null;
   }
 
   public async result_array() {
