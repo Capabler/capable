3ccb5150c134ed1f28530b8ca30c6d4d39007eaa
topthinking <topthinking@ximalaya.com>
2018-10-29 18:40:31 +0800

    example

--- example/config/config.js
+++ example/config/config.js
@@ -1,11 +1,6 @@
-const path = require('path');
-const ejsView = require('koa-ejs');
-const reactView = require('koa-react-view');
-const dir = process.cwd();
-
 module.exports = {
   // 服务端口号
-  port: 3001,
+  port: 3000,
 
   // 数据库引擎可选, lokijs、sequelize
   database_engine: 'lokijs',
@@ -20,58 +15,6 @@ module.exports = {
   subclass_prefix: 'MY_',
 
   /**
-   * 自定义静态资源存放目录
-   */
-  source: path.join(__dirname, '../views/public'),
-
-  /**
-   * 使用的模板引擎
-   * 需要注意这里面的引擎需要和template.js中的配置一致
-   * 比如：这里是react模板渲染，template配置需要是react-view模板配置项
-   * ⚠️ react模板仅仅支持静态资源导出，如果绑定事件处理的不推荐使用这一套web框架
-   * 针对于react模板 我们推荐使用前后端分离框架去完整的实现一套服务端渲染架构
-   * 当前框架仅仅是服务于 web业务层，提供接口服务的。表现层不参与深度的解析工作
-   *
-   * 引擎列表[react,ejs]
-   * 这边是默认的模板引擎，可用通过this.load.template(tpl)动态改变模板引擎
-   * 系统默认的是当前设置的引擎
-   * 动态改变需注意，必须在模板列表中配置存在的
-   */
-  template_engine: 'ejs',
-
-  /**
-   * 可用模板列表
-   * render: 模板render方法
-   * ext: 模板后缀
-   */
-  templates: {
-    react: {
-      render(app) {
-        require('babel-register')({
-          presets: ['es2015', 'react'],
-          extensions: ['.jsx'],
-        });
-        reactView(app, {
-          views: path.join(dir, 'views'),
-        });
-      },
-      ext: 'jsx',
-    },
-    ejs: {
-      render(app) {
-        ejsView(app, {
-          root: path.join(dir, 'views'),
-          layout: false,
-          viewExt: 'ejs',
-          cache: false,
-          debug: false,
-        });
-      },
-      ext: 'ejs',
-    },
-  },
-
-  /**
    * 加载一些model全局使用，不需要每次都去this.load.model()
    * 接受多个model的加载，数组形式
    * 内容和this.load.model的传值的信息一致，都是models文件夹下的文件的名称，不包括后缀
@@ -79,4 +22,9 @@ module.exports = {
    * models:['book_model','user_model']
    */
   models: [],
+
+  /**
+   * 是否使用socket
+   */
+  socket: false,
 };
--- example/controllers/index.js
+++ example/controllers/index.js
@@ -1,27 +1,5 @@
-module.exports = class extends MY_Controller { 
-	
-	async index() {
-		await this.method('post', 'get')
-		
-		await this.method.get(async () => {
-			let data = {
-				isLogin: this.isLogin
-			}
-			this.load.model('user_model')
-			this.load.model('book_model')
-			data['userList'] = await this.user_model.getuserList()
-			data['bookList'] = await this.book_model.bookList()
-
-			//默认模板引擎是ejs，动态加载react模板引擎，用来渲染该页面
-			if ($_SESSION['user_name'] === 'root') {
-				// 只有 root 用户登录使用该引擎
-				this.load.template('react')
-			}
-			await this.view('index', data)
-		})
-
-		await this.method.post(async () => { 
-			this.ctx.body = '123'
-		})
-	}		
-}
+module.exports = class extends DJ_Controller {
+  async index() {
+    this.ctx.body = 'hello delicatejs';
+  }
+};
